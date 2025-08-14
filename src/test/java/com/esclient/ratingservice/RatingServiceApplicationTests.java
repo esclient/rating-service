29f88f686dba00a39ccdde585b18e935f6316c0a
Samuel <samueldanielyan@gmail.com>
2025-08-15 00:36:01 +0200

    test

--- src/test/java/com/esclient/ratingservice/RatingServiceApplicationTests.java
+++ src/test/java/com/esclient/ratingservice/RatingServiceApplicationTests.java
@@ -3,10 +3,14 @@ package com.esclient.ratingservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration;
 
-@SpringBootTest(exclude = net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration.class)
+@SpringBootTest
+@ImportAutoConfiguration(exclude = GrpcServerAutoConfiguration.class) // Exclude gRPC server auto-config
 @ActiveProfiles("test")
 class RatingServiceApplicationTests {
+
     @Test
     void contextLoads() {}
 }
