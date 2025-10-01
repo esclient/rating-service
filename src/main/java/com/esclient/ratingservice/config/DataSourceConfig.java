3539d1a66c11e8c270564c85e9ac19e1561c383d
Samuel <samueldanielyan@gmail.com>
2025-10-01 23:48:14 +0200

    fully optimized testing environment
    FULL database testing(including invalid and valid values, primary key violations, validation errors, functionality test(getRatings() and rateMod()), added testing sql schema, adjusted and disabled for testing DataSourceConfig.java and added TestDataSourceConfig.java as a mock-config

--- src/main/java/com/esclient/ratingservice/config/DataSourceConfig.java
+++ src/main/java/com/esclient/ratingservice/config/DataSourceConfig.java
@@ -9,9 +9,11 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
+@Profile("!test")
 public class DataSourceConfig {
 
   // Static fields first
