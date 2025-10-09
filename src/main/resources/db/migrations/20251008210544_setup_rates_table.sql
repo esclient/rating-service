1aac624799b70c25feefeebde385f11e866d101d
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-10-09 14:19:00 +0300

    Update 20251008210544_setup_rates_table.sql

--- src/main/resources/db/migrations/20251008210544_setup_rates_table.sql
+++ src/main/resources/db/migrations/20251008210544_setup_rates_table.sql
@@ -1,27 +1,17 @@
 -- +goose Up
--- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS public.rates (
+CREATE TABLE IF NOT EXISTS rates (
     id BIGSERIAL PRIMARY KEY,
     author_id BIGINT NOT NULL,
     mod_id BIGINT NOT NULL,
     rate BIGINT NOT NULL
 );
--- +goose StatementEnd
 
--- +goose StatementBegin
-CREATE INDEX IF NOT EXISTS idx_rates_author_id ON public.rates(author_id);
--- +goose StatementEnd
-
--- +goose StatementBegin
-CREATE INDEX IF NOT EXISTS idx_rates_mod_id ON public.rates(mod_id);
--- +goose StatementEnd
-
--- +goose StatementBegin
-CREATE INDEX IF NOT EXISTS idx_rates_mod_author ON public.rates(mod_id, author_id);
--- +goose StatementEnd
+CREATE INDEX IF NOT EXISTS idx_rates_author_id ON rates(author_id);
+CREATE INDEX IF NOT EXISTS idx_rates_mod_id ON rates(mod_id);
+CREATE INDEX IF NOT EXISTS idx_rates_mod_author ON rates(mod_id, author_id);
 
 -- +goose Down
-DROP INDEX IF EXISTS public.idx_rates_mod_author;
-DROP INDEX IF EXISTS public.idx_rates_mod_id;
-DROP INDEX IF EXISTS public.idx_rates_author_id;
-DROP TABLE IF EXISTS public.rates;
+DROP INDEX IF EXISTS idx_rates_mod_author;
+DROP INDEX IF EXISTS idx_rates_mod_id;
+DROP INDEX IF EXISTS idx_rates_author_id;
+DROP TABLE IF EXISTS rates;
